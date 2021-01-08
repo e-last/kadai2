7d9284ab70aa6d409e3603a47619d34b94dd9b64
e-last <statw54@gmail.com>
2021-01-08 09:08:06 +0000

    [kadai3]

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,10 @@ Rails.application.routes.draw do
   get "home/about" => "homes#about"
 
 
-  resources :books, only: [ :create, :index, :show, :edit, :update, :destroy]
+  resources :books, only: [ :create, :index, :show, :edit, :update, :destroy] do
+    resources :book_comments, only: [:create, :destroy]
+    resource :favorites, only: [:create, :destroy]
+  end
 
   resources :users, only: [:index, :show, :edit, :update]
   
