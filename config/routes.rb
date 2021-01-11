c129c7262185657ee02f65c2ad44dfc609540d58
e-last <statw54@gmail.com>
2021-01-11 15:25:37 +0000

    [kadai5]

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   
+  get 'search/search'
   devise_for :users
   root to: 'homes#top'
 
@@ -16,5 +17,7 @@ Rails.application.routes.draw do
     get :follows, on: :member 
     get :followers, on: :member
   end
+  
+  get '/search' => 'search#search'
   
 end
